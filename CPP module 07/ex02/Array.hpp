b3bec2975ec3edb0ece15c960d600b24248113ee
Дарья Паркина <lkasandr@MacBook-Pro-Dara.local>
2021-09-18 23:36:07 +0300

    add CPP07

--- CPP module 07/ex02/Array.hpp
+++ CPP module 07/ex02/Array.hpp
@@ -7,38 +7,75 @@ template <class T>
 class Array
 {
     private:
-        unsigned int lenght;
         T* data;
+        unsigned int n;
     
     public:
-        Array(void) : lenght(0), data(NULL) {};
-        Array(unsigned int lenght) : lenght(lenght)
+        Array(void) : data(NULL), n(0) {};
+
+        Array(unsigned int n) : n(n)
         {
-            this->data = new T[lenght];
+            this->data = new T[n];
         };
-        Array(const T &copy)
+
+        Array(const Array<T> &copy)
         {
+            this->data = NULL;
             *this = copy;
         };
-        Array& operator=(const T &copy)
+
+        Array& operator=(const Array<T> &copy)
         {
             if (this == &copy)
                 return *this;
-            this->lenght = copy.lenght;
+            this->n = copy.n;
             if (this->data)
                 delete [] this->data;
-            this->data = new T[this->lenght];
-            for (int i = 0; i < this->lenght; i++)
-                this->data[i] = copy[i];
+            this->data = new T[this->n];
+            for (unsigned int i = 0; i < this->n; i++)
+                this->data[i] = copy.data[i];
             return *this;
         };
+
+        class indexException : public std::exception
+        {
+            public:
+			    const char* what() const throw()
+                {
+                    return "wrong index!";
+                }
+        };
+
+        T& operator[](unsigned int n)
+        {
+            if (n >= this->n || n < 0)
+                throw indexException();
+            else
+                return (this->data[n]);
+        };
+
+        const T& operator[](unsigned int n) const 
+        {
+            if (n >= this->n || n < 0)
+                throw indexException();
+            else
+                return (this->data[n]);
+        };
+
         ~Array(void)
         {
             if (this->data)
                 delete [] this->data;
             this->data = NULL;
         };
+
+        unsigned int size() const
+        { 
+            return this->n;
+        };
         
 };
+
+
 
 #endif
--- CPP module 07/ex02/main.cpp
+++ CPP module 07/ex02/main.cpp
@@ -1,31 +1,73 @@
-#include "iter.hpp"
+#include "Array.hpp"
 
-class Awesome
+#define MAX_VAL 750
+
+int main(int, char**)
 {
-	public:
-		Awesome(void): _n( 42 ) { return; }
-		int get(void) const { return this->_n; }
-	private:
-		int _n;
-};
+    Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
 
-std::ostream & operator<<(std::ostream& o, Awesome const & rhs) 
-{ 
-    o << rhs.get(); return o; 
-}
+	for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    //SCOPE
+	// std::cout << "Numbers's size: " << numbers.size() << std::endl;
+    {
+        Array<int> tmp = numbers;
+		// std::cout << "Tmp's size: " << tmp.size() << std::endl;
+        Array<int> test(tmp);
+		// std::cout << "test's size: " << test.size() << std::endl;
+    }
 
-template <typename T>
-void print(T const & x) 
-{ 
-    std::cout << x << std::endl; return; 
-}
+	// Array<int> tmp2;
 
-int main() 
-{
-	int tab[] = {0, 1, 2, 3, 4};
-	Awesome tab2[5];
+	// std::cout << "tmp2's size: " << tmp2.size() << std::endl;
+	
+    try
+    {
+    //    std::cout << "test wrong index: " << std::endl;
+	   numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        // std::cout << "test wrong index: " << std::endl;
+		numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 
-	iter(tab, 5, print);
-	iter(tab2, 5, print);
+	try
+    {
+        numbers[10] = 0;
+		// std::cout << "test for right index: " << numbers[10] << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;//
     return 0;
 }
